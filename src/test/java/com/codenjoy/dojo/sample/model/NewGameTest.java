2db33d616300db63d2b2c680b8db2e0530001d85
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-05-15 13:55:23 +0300

    [engine][sample] Refactoring in test - prepare abstract part. Fixed tests in engine.

--- src/test/java/com/codenjoy/dojo/sample/model/NewGameTest.java
+++ src/test/java/com/codenjoy/dojo/sample/model/NewGameTest.java
@@ -26,17 +26,19 @@ import com.codenjoy.dojo.client.local.DiceGenerator;
 import com.codenjoy.dojo.sample.TestGameSettings;
 import com.codenjoy.dojo.sample.services.GameRunner;
 import com.codenjoy.dojo.sample.services.GameSettings;
-import com.codenjoy.dojo.services.Deal;
-import com.codenjoy.dojo.services.Deals;
-import com.codenjoy.dojo.services.Dice;
-import com.codenjoy.dojo.services.PlayerSave;
+import com.codenjoy.dojo.services.*;
 import com.codenjoy.dojo.services.multiplayer.FieldService;
 import com.codenjoy.dojo.services.multiplayer.LevelProgress;
 import com.codenjoy.dojo.services.multiplayer.Spreader;
 import com.codenjoy.dojo.services.room.RoomService;
+import com.codenjoy.dojo.utils.smart.SmartAssert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static org.junit.Assert.assertEquals;
@@ -44,13 +46,56 @@ import static org.junit.Assert.assertNotNull;
 
 public class NewGameTest {
 
+    private Deals all;
+    private Dice dice;
+    private RoomService rooms;
+
+    private List<Deal> deals = new LinkedList<>();
+
+    @Before
+    public void before() {
+        rooms = new RoomService();
+        FieldService fields = new FieldService(0);
+        Spreader spreader = new Spreader(fields);
+
+        all = new Deals(spreader, rooms);
+        ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+        all.init(lock);
+        all.onAdd(deal -> assertNotNull(deal));
+        all.onRemove(deal -> assertNotNull(deal));
+
+        dice = new DiceGenerator(message -> System.out.println(message)).getDice();
+    }
+
     @Test
     public void test() {
         // given
-        RoomService rooms = new RoomService();
-        FieldService fields = new FieldService(0);
-        Spreader spreader = new Spreader(fields);
-        Dice dice = new DiceGenerator(message -> System.out.println(message)).getDice();
+        givenFl("☼☼☼☼☼\n" +
+                "☼   ☼\n" +
+                "☼ ☺ ☼\n" +
+                "☼   ☼\n" +
+                "☼☼☼☼☼\n");
+
+        // then
+        assertF("☼☼☼☼☼\n" +
+                "☼   ☼\n" +
+                "☼☺  ☼\n" +
+                "☼   ☼\n" +
+                "☼☼☼☼☼\n");
+
+        // when
+        hero().up();
+        tick();
+
+        // then
+        assertF("☼☼☼☼☼\n" +
+                "☼☺  ☼\n" +
+                "☼   ☼\n" +
+                "☼   ☼\n" +
+                "☼☼☼☼☼\n");
+    }
+
+    private void givenFl(String... map) {
         GameRunner gameType = new GameRunner(){
             @Override
             public Dice getDice() {
@@ -62,34 +107,67 @@ public class NewGameTest {
                 int level = LevelProgress.levelsStartsFrom1;
                 return new TestGameSettings()
                         .clearLevelMaps(level)
-                        .setLevelMap(level,
-                                "☼☼☼☼☼\n" +
-                                "☼   ☼\n" +
-                                "☼ ☺ ☼\n" +
-                                "☼   ☼\n" +
-                                "☼☼☼☼☼\n");
+                        .setLevelMaps(level, map);
             }
         };
 
-        Deals deals = new Deals(spreader, rooms);
-        ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
-        deals.init(lock);
-        deals.onAdd(deal -> assertNotNull(deal));
-        deals.onRemove(deal -> assertNotNull(deal));
         long now = Calendar.getInstance().getTimeInMillis();
 
-        // when
-        Deal deal = deals.deal(PlayerSave.NULL, "room", "player", "callbackUrl", gameType, now);
+        rooms.create("room", gameType);
+        Deal deal = all.deal(PlayerSave.NULL, "room", "player", "callbackUrl", gameType, now);
 
-        deal.tick();
-
-        // then
-        assertEquals("☼☼☼☼☼\n" +
-                    "☼   ☼\n" +
-                    "☼☺  ☼\n" +
-                    "☼   ☼\n" +
-                    "☼☼☼☼☼\n",
-                deal.getGame().getBoardAsString(true));
+        deals.add(deal);
     }
+
+    private void tick() {
+        all.tick();
+    }
+
+    @After
+    public void after() {
+        SmartAssert.checkResult();
+    }
+
+    // public getters
+
+    public Deal deal() {
+        return deal(0);
+    }
+
+    public Deal deal(int index) {
+        return deals.get(index);
+    }
+
+    public Game game() {
+        return game(0);
+    }
+
+    public Game game(int index) {
+        return deal(index).getGame();
+    }
+
+    public Joystick hero() {
+        return deal().getJoystick();
+    }
+
+    public Joystick hero(int index) {
+        return deal(index).getJoystick();
+    }
+
+    // common asserts
+
+    public void assertF(String expected) {
+        assertF(expected, 0);
+    }
+
+    /**
+     * Проверяет одну борду с заданным индексом.
+     * @param expected Ожидаемое значение.
+     * @param index Индекс игрока.
+     */
+    public void assertF(String expected, int index) {
+        assertEquals(expected, game(index).getBoardAsString(true));
+    }
+
 
 }
